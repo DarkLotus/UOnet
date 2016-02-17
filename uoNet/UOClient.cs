d33603b31c96221443d6108aacc635ff8c358170
James Kidd <kklolk@gmail.com>
2016-02-18 00:04:40 +1100

    Added SmartMove, uses Basic Pathfinding then uses uo's pathfind/move.

---
+++ uoNet/UOClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uoNet
+{
+    public static class UOHELPERS
+    {
+        public static void SmartMove(this UO t, Vector3 v,int accuracy = 0)
+        {
+            var path = FindPath(t, new Vector3(t.CharPosX, t.CharPosY), v,accuracy);
+
+            for(int i = 0; i < path.Count;i++)
+            {
+                if (i + 3 < path.Count -1)
+                    i = i + 3;
+                var p = path[i];
+                t.PathFind(p.X, p.Y, 0);//, 2000);
+                t.Wait(5);
+                t.Move(p.X, p.Y, 0, 2000);
+            }
+
+        }
+       
+
+        private static List<Vector3> FindPath(UO t, Vector3 start, Vector3 dest,int accuracy = 0)
+        {
+            var ClosedSet = new List<Vector3>();
+            var OpenSet = new List<Vector3>();
+            OpenSet.Add(start);
+
+            var CameFrom = new List<Vector3>();
+            Vector3 curNode = null;
+
+            while (OpenSet.Count > 0 )
+            {
+                curNode = OpenSet.First();
+                OpenSet.RemoveAt(0);
+
+                if (curNode.Equals(dest))
+                    break;
+                var neighbours = GetNeighbours(curNode,dest);
+                foreach(var n in neighbours)
+                {
+                    if (accuracy > 0)
+                        if (n.Equals(dest))
+                        {
+                            OpenSet.Clear();
+                            break;
+                        }
+                            
+                    if (!OpenSet.Contains(n) && !ClosedSet.Contains(n) && n.IsPassable())
+                    {
+                        OpenSet.Add(n);
+                    }
+                }
+                OpenSet.Sort();
+                ClosedSet.Add(curNode);
+            }
+
+            var resultPath = new List<Vector3>();
+            //curnode is Start
+            while(curNode != null)
+            {
+                resultPath.Add(new Vector3(curNode.X, curNode.Y));
+
+
+                //if (curNode.P?.P?.P != null)
+              //      curNode = curNode.P?.P?.P;
+              //  else
+                    curNode = curNode.P;
+
+
+            }
+            resultPath.Reverse();
+            return resultPath;
+
+        }
+
+        private static List<Vector3> GetNeighbours(Vector3 curNode, Vector3 dest)
+        {
+            var results = new List<Vector3>();
+            for(int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+                   // if (Math.Abs(x) == Math.Abs(y))
+                    //    continue;
+                    var heuristc = Tools.Get2DDistance(curNode.X + x, curNode.Y + y, dest.X, dest.Y);
+                        results.Add(new Vector3(curNode.X + x, curNode.Y + y) { V = heuristc, P = curNode});
+                }
+            }
+            return results;
+        }
+    }
+}
--- uoNet/Vector3.cs
+++ uoNet/Vector3.cs
@@ -1,8 +1,12 @@
-﻿namespace uoNet
+﻿using System;
+
+namespace uoNet
 {
-    public class Vector3
+    public class Vector3 : IComparable
     {
         public int X, Y, Z;
+        public int V;
+        public Vector3 P;
 
         public Vector3(int x, int y, int z = 0)
         {
@@ -11,6 +15,50 @@
             Z = z;
         }
 
-        
+        bool? isPassable = null;
+        internal bool IsPassable()
+        {
+            if(!isPassable.HasValue)
+            {
+                isPassable = true;
+                var land = Ultima.Map.Felucca.Tiles.GetLandTile(X, Y);
+                var staticTile = Ultima.Map.Felucca.Tiles.GetStaticTiles(X, Y);
+                if(land.Z < 25 && Ultima.TileData.LandTable[land.ID].Flags.HasFlag(Ultima.TileFlag.Impassable))
+                {
+                    isPassable = false;
+                    return isPassable.Value;
+                }
+
+                foreach(var t in staticTile)
+                {
+                    if (t.Z < 12 && Ultima.TileData.ItemTable[t.ID].Flags.HasFlag(Ultima.TileFlag.Impassable))
+                    {
+                        isPassable = false;
+                        return isPassable.Value;
+                    }
+
+                }
+                if (land.Z > 25 && staticTile.Length == 0)
+                    return false;
+            }
+            return isPassable.Value;
+          
+        }
+
+        public override bool Equals(object obj)
+        {
+            Vector3 other = obj as Vector3;
+            return other.X == X && other.Y == Y;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Vector3 other = obj as Vector3;
+            if (other.V > V)
+                return -1;
+            if (other.V < V)
+                return 1;
+            return 0;
+        }
     }
 }
