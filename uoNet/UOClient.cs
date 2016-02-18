ce0281a35c5180c555d5ec8a8f74bcb33d6622f0
James Kidd <kklolk@gmail.com>
2016-02-18 19:31:38 +1100

    Pathfinding evolved again, closer to A* now.

--- uoNet/UOClient.cs
+++ uoNet/UOClient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 
@@ -9,17 +11,40 @@ namespace uoNet
     {
         public static bool SmartMove(this UO t, Vector3 v,int accuracy = 0)
         {
+
+           /*Bitmap bmp = new Bitmap(4096, 4096);
+            for(int x = 2300;x < 2700; x++)
+            {
+                for(int y = 0;y < 600; y++)
+                {
+                    var tile = new Vector3(x, y);
+                    bmp.SetPixel(x, y, tile.IsPassable() ? Color.Green : Color.Red);
+                }
+            }
+            bmp.Save("testsm.png",ImageFormat.Png);
+            var vv = new Vector3(2475, 431).IsPassable();*/
             var path = FindPath(t, new Vector3(t.CharPosX, t.CharPosY), v,accuracy);
             if (path == null)
                 return false;
+
+            int timoutCnt = 3000;
+            var timer = System.Diagnostics.Stopwatch.StartNew();
             for(int i = 1; i < path.Count;i++)
             {
                 if (i + 3 < path.Count -1)
                     i = i + 3;
                 var p = path[i];
                 t.PathFind(p.X, p.Y, 0);//, 2000);
-                t.Wait(5);
-                t.Move(p.X, p.Y, 0, 5000);
+                while (timer.ElapsedMilliseconds < timoutCnt && t.CharPosX != p.X && t.CharPosY != p.Y)
+                {
+                    System.Threading.Thread.Sleep(5);
+                    //t.PathFind(p.X, p.Y, 0);//, 2000);
+                }
+                    
+                timer.Restart();
+                //t.Wait(5);
+                if (t.CharPosX != p.X && t.CharPosY != p.Y)
+                    t.Move(p.X, p.Y, 0, 5000);
             }
             return true;
 
@@ -28,7 +53,8 @@ namespace uoNet
 
         private static List<Vector3> FindPath(UO t, Vector3 start, Vector3 dest,int accuracy = 0)
         {
-            var ClosedSet = new List<Vector3>();
+            var closedSet = new Vector3[4096,4096];
+           // var ClosedSet = new List<Vector3>();
             var OpenSet = new List<Vector3>();
             OpenSet.Add(start);
 
@@ -51,32 +77,29 @@ namespace uoNet
                             OpenSet.Clear();
                             break;
                         }
-                            
-                    if (!OpenSet.Contains(n) && !ClosedSet.Contains(n) && n.IsPassable())
+                    //!ClosedSet.Contains(n)
+                    if (!OpenSet.Contains(n) && closedSet[n.X,n.Y] == null && n.IsPassable())
                     {
                         OpenSet.Add(n);
                     }
                 }
                 OpenSet.Sort();
-                ClosedSet.Add(curNode);
-                if (ClosedSet.Count > 25000)
-                    return null;
+                closedSet[curNode.X, curNode.Y] = curNode;
+               // ClosedSet.Add(curNode);
+               // if (ClosedSet.Count > 50000)
+                //    return null;
             }
 
             var resultPath = new List<Vector3>();
             //curnode is Start
-            while(curNode != null)
+            //Bitmap bmp = new Bitmap(4096, 4096);
+            while (curNode != null)
             {
+                //bmp.SetPixel(curNode.X, curNode.Y, Color.Red);
                 resultPath.Add(new Vector3(curNode.X, curNode.Y));
-
-
-                //if (curNode.P?.P?.P != null)
-              //      curNode = curNode.P?.P?.P;
-              //  else
-                    curNode = curNode.P;
-
-
+                curNode = curNode.P;
             }
+            //bmp.Save("path.png", ImageFormat.Png);
             resultPath.Reverse();
             return resultPath;
 
@@ -94,7 +117,7 @@ namespace uoNet
                    // if (Math.Abs(x) == Math.Abs(y))
                     //    continue;
                     var heuristc = Tools.Get2DDistance(curNode.X + x, curNode.Y + y, dest.X, dest.Y);
-                        results.Add(new Vector3(curNode.X + x, curNode.Y + y) { V = heuristc, P = curNode});
+                        results.Add(new Vector3(curNode.X + x, curNode.Y + y) { V = heuristc + curNode.V, P = curNode});
                 }
             }
             return results;
